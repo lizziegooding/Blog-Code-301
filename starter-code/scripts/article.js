283c8d181b50f8f8745caa1a2b02e6a43f4c483e
Michael Freeman <mjfreem1@gmail.com>
2016-06-07 13:57:25 -0700

    first commit with html and articles.js finished

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,33 +1,43 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the js object passed in to complete this contructor function:
+  // DONE: Use the js object passed in to complete this contructor function:
   // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
   this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
-Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
+Article.prototype.toHtml = function() { //creates a new method toHtml for all article objs
+  var $newArticle = $('article.template').clone(); //clones articles and all descendants within HTML
 
-  $newArticle.attr('data-category', this.category);
+  $newArticle.attr('data-category', this.category); //assigns clone a custon attribute of data-category and assigns
 
-  // TODO: Use jQuery to fill in the template with properties
+  // DONE: Use jQuery to fill in the template with properties
   // from this particular Article instance. We need to fill in:
   // the author name and url, the article title and body, and the
   // publication date.
-
+  $newArticle.$('h1').text(this.title);
+  $newArticle.$('a').text(this.author);
+  $newArticle.$('a').attr('href', this.authorUrl);
+  $newArticle.$('time').text(this.publishedOn);
+  $newArticle.$('section.article-body').text(this.body);
   // Include the publication date as a 'title' attribute to show on hover:
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
 
   // Display the date as a relative number of "days ago":
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
 
   $newArticle.append('<hr>');
 
-  // TODO: This cloned article is no longer a template, so we should remove that class...
+  // DONE: This cloned article is no longer a template, so we should remove that class...
+  $newArticle.$('article').removeClass('template');
 
   return $newArticle;
-}
+};
 
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
@@ -35,8 +45,8 @@ rawData.sort(function(a,b) {
 
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-})
+});
 
 articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
+  $('#articles').append(a.toHtml());
 });
